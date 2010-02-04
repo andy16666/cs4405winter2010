dca539ec61abfdb355ab5469ee03ab68f75e107c
andy16666 <andy16666@0400735a-0b4c-11df-a6e1-f7bfe9b6b944>
2010-02-04 03:25:28 +0000

    Minor changes following rewrite.

--- trunk/lcd.c
+++ trunk/lcd.c
@@ -4,69 +4,67 @@
  *  Template for using the LCD with the robot.
  */
 
-
 #include "interrupts.h"
 #include "lcd.h"
 
-#define MAX_CHAR_COUNT 16
-#define RESETV	(*(volatile int *)(0xBFC0 + 0x3E))
+#define MAX_CHAR_COUNT 16 
+#define RESETV	(*(interrupt_t *)(0xBFC0 + 0x3E))
 
-extern void _Main();
-  
+
+void sys_send_command_lcd(unsigned short operation, unsigned short operand) {
+	asm volatile(
+		"ldx	#4096        \n"  /* $1000 = Port-base.  */ 
+		"bclr	0,X	#16  \n"
+		"bclr	60,X	#32  \n"
+		"bclr	0,X	#16  \n"
+		"ldaa	#255         \n"
+		"staa	7,X          \n"
+		"ldaa	%[operation] \n"
+		"staa	4,X          \n"          
+		"ldab	%[operand]   \n"         
+		"stab	3,X          \n"
+		"bset	0,X #16      \n"
+		"bclr	0,X #16      \n"
+		"clr	7,X          \n"
+	"wait:	ldaa	#1           \n"
+		"staa	4,X          \n"
+		"bset	0,X #16      \n"
+		"ldab	3,X          \n"
+		"bclr	0,X #16      \n"
+		"andb	#128         \n"
+		"beq	wait         \n"  
+	"Done:	bset	60,X	#32  \n"
+		: 
+		: [operation] "m" (&operation), [operand] "m" (&operand) 
+		: "x","y","a","b","memory"   
+	);
+}
+
 void sys_print_lcd(char* text) {
-	unsigned char i = 0;
+	int i;
         unsigned int k;
-
-	_sys_send_command_lcd(0,3); 
-
-	for (k = 1; k != 0; k++);
 	
-	while (*text != 0 && i < MAX_CHAR_COUNT) {
-		_sys_send_command_lcd(2,*text); 
-		text++;
-		i++;
-	}
+	sys_send_command_lcd(0,3); /* Clear The LCD?? */ 
+
+	for (k = 1; k != 0; k++); 
 	
+	for (i = 0; text[i] && i < MAX_CHAR_COUNT; i++) {
+		sys_send_command_lcd(2,(unsigned short)text[i]);
+	}	
 }
 
-static void _sys_send_command_lcd(unsigned short operation, unsigned short operand) {
-	asm volatile("
-		ldx		#4096          ; $1000...PORTBASE
-		bclr	0,X	#16
-		bclr	60,X	#32
-		bclr	0,X		#16
-		ldaa	#255           ; #$ff
-		staa	7,X
-		ldaa	%0            ; $fe 
-		staa	4,X            
-		ldab	%1            ; $ff
-		stab	3,X
-		bset	0,X		#16
-		bclr	0,X		#16
-		clr		7,X
-wait:	ldaa	#1
-		staa	4,X
-		bset	0,X		#16
-		ldab	3,X
-		bclr	0,X		#16
-		andb	#128
-		beq		wait
-		
-Done:	bset	60,X	#32
-	" : : "m" (operation), "m" (operand) : "X","Y","A","B","memory");
-}
-
-void _sys_init_lcd(void) {	
-	_sys_send_command_lcd(0,15);
+void sys_init_lcd(void) {	
+	sys_send_command_lcd(0,15);
 }
 
 void _Main(void)
 {
-  char *ken = "Ken!\0"; 
-  char *joey = "Joey\0";
+  char *ken  = "Ken!"; 
+  char *joey = "Joey";
   unsigned int i;
 
-  _sys_init_lcd();
+  sys_init_lcd();
+
   while (1)
   {
     sys_print_lcd(ken);
@@ -94,8 +92,8 @@ void _Main(void)
  * int:  some ANSI spec requires a return from main 
  */
 int main(void) {
-	RESETV = (unsigned int)&_Main; 	/* register the reset handler */
-	while(1);			            /* hang around */
+	RESETV = (interrupt_t)&_Main; 	/* register the reset handler */
+	while(1);			/* hang around */
 	return 0;	
 }
 
