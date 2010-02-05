a7ce01b5de52e9e2b3409f90122660ad13de4ad8
andy16666 <andy16666@0400735a-0b4c-11df-a6e1-f7bfe9b6b944>
2010-02-05 14:39:56 +0000

    Fixed compile errors.

--- trunk/lcd.c
+++ trunk/lcd.c
@@ -4,34 +4,34 @@
  *  Template for using the LCD with the robot.
  */
 
-#include "interrupts.h"
+#include "interrupts.c"
 #include "lcd.h"
 #include "ports.h"
 
 #define MAX_CHAR_COUNT 16
 
 void sys_send_command_lcd(unsigned char operation, unsigned char operand) {
-	Port[M6811_PORTA] CLR_BIT(M6811_BIT4); 
-	Port[M6811_HPRIO] CLR_BIT(M6811_BIT5); /* BIT5 = MDA */  
-	Port[M6811_PORTA] CLR_BIT(M6811_BIT4); 
+	Ports[M6811_PORTA] CLR_BIT(M6811_BIT4); 
+	Ports[M6811_HPRIO] CLR_BIT(M6811_BIT5); /* BIT5 = MDA */  
+	Ports[M6811_PORTA] CLR_BIT(M6811_BIT4); 
 
-	Port[M6811_DDRC]   = 255;
+	Ports[M6811_DDRC]   = 255;
 
-	Port[M6811_PORTB]  = operation;
-	Port[M6811_PORTCL] = operand;  
+	Ports[M6811_PORTB]  = operation;
+	Ports[M6811_PORTCL] = operand;  
 	
-	Port[M6811_PORTA] SET_BIT(M6811_BIT4); 
-	Port[M6811_PORTA] CLR_BIT(M6811_BIT4); 
+	Ports[M6811_PORTA] SET_BIT(M6811_BIT4); 
+	Ports[M6811_PORTA] CLR_BIT(M6811_BIT4); 
        
 	do {  /* wait */ 
-		Port[M6811_DDRC]  = 0; 
-		Port[M6811_PORTB] = 1; 
+		Ports[M6811_DDRC]  = 0; 
+		Ports[M6811_PORTB] = 1; 
 
-		Port[M6811_PORTA] SET_BIT(M6811_BIT4)
-		Port[M6811_PORTA] CLR_BIT(M6811_BIT4)
-	} while ((Port[M6811_PORTC] & M6811_BIT7) == 1);
+		Ports[M6811_PORTA] SET_BIT(M6811_BIT4);
+		Ports[M6811_PORTA] CLR_BIT(M6811_BIT4);
+	} while ((Ports[M6811_PORTC] & M6811_BIT7) == 1);
 
-	Port[M6811_HPRIO] SET_BIT(M6811_BIT5); /* BIT5 = MDA */  
+	Ports[M6811_HPRIO] SET_BIT(M6811_BIT5); /* BIT5 = MDA */  
 }
 
 void sys_print_lcd(char* text) {
