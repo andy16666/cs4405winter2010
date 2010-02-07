09e9447235ac048829b439e55f359b15f1d8a71d
andy16666 <andy16666@0400735a-0b4c-11df-a6e1-f7bfe9b6b944>
2010-02-07 19:12:28 +0000

    Adding Semaphores.

---
+++ trunk/semaphore.h
@@ -0,0 +1,18 @@
+/*
+ * fifo.h
+ * An implementation of a FIFO data structure.  This file contains the 
+ * structure definitions and the function prototypes.
+ *
+ * Authors: 
+ *	Joel Goguen <r1hh8@unb.ca>
+ *	Andrew Somerville <z19ar@unb.ca>
+ */
+
+#ifndef __SEMAPHORE_H__
+#define __SEMAPHORE_H__
+
+#include "os.h"
+
+extern int Semaphores[MAXSEM];
+
+#endif /* __SEMAPHORE_H__ */
